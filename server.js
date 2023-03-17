c9fe1219e1fd8a82f1bf73b5376ecf2f8ce9c58e
Naveen Sharma <nav.shar000@gmail.com>
2023-03-17 13:04:55 -0400

    add mongoDB database connection in config file with URI stored in dotenv

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT 
 const cors = require("cors");
 const routes = require('./routes/routes');
+const connectDB = require("./config/db");
 
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.use('/api/acronyms', routes);
 
