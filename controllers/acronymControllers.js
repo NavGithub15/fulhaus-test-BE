d08b9807fd4466bacd95741da4806a0f94c2a83f
Naveen Sharma <nav.shar000@gmail.com>
2023-03-17 15:57:52 -0400

    Add logic for pagination and search

--- controllers/acronymControllers.js
+++ controllers/acronymControllers.js
@@ -1,10 +1,39 @@
 const Acronym = require("../models/acronymModels");
 const asyncHandler = require("express-async-handler");
+const Fuse = require("fuse.js");
 
-// gets the acronyms from the database
+// get all acronyms from the database and performs a fuzzy search and pagination
 const getAcronym = asyncHandler(async (req, res) => {
-  const acronyms = await Acronym.find({});
-  res.json(acronyms);
+  try {
+    // extract query parameters from the request object
+    const page = parseInt(req.query.page) || 1; // default page is 1
+    const limit = parseInt(req.query.limit) || 10; // default limit is 10
+    const search = req.query.search; // search term
+
+    // create a query object to find acronyms in the database
+    let query = {};
+
+    // if search term is provided, create a regular expression to search for fuzzy matches
+    if (search) {
+      const regex = new RegExp(search, "i");
+      query = { acronym: regex };
+    }
+
+    // find acronyms in the database based on the query object and pagination parameters
+    const acronyms = await Acronym.find(query)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    // find total count of acronyms in the database based on the query object
+    const totalCount = await Acronym.countDocuments(query);
+
+    // set response header to indicate total count of acronyms in the database
+    res.set("X-Total-Count", totalCount.toString());
+
+    res.status(200).json(acronyms);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get acronyms", error });
+  }
 });
 
 // gets the acronym by id
@@ -28,7 +57,9 @@ const createAcronym = asyncHandler(async (req, res) => {
   const newAcronym = new Acronym(req.body);
   try {
     const savedAcronym = await newAcronym.save();
-    res.status(201).json({message: "Acronym created successfully", savedAcronym});
+    res
+      .status(201)
+      .json({ message: "Acronym created successfully", savedAcronym });
   } catch (error) {
     res.status(500).json({ message: "Failed to create acronym", error });
   }
